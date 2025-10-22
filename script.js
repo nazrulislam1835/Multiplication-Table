50ad232a8e7115ffecc067e60db950e5451b48b7
Nazrulislam1835 <nazrulislam1835@gmail.com>
2025-10-22 20:41:56 +0600

    Full Project

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let i
+let heading = document.querySelector(".heading");
+let input = document.querySelector(".input");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  heading.innerHTML = "";
+
+  for (let i = 1; i < 11; i++) {
+    heading.innerHTML += `${input.value} x ${i} = ${input.value * i} <br>`;
+  }
+});
